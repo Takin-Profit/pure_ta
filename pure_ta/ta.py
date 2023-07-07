987ee63a34efbc22359d74653c2f31fef5da5aa9
g5becks <techstar.dev@hotmail.com>
2023-07-07 03:20:29 -0400

    test(Test): add alma tests
    add tests for Ta.alma

--- pure_ta/ta.py
+++ pure_ta/ta.py
@@ -1,11 +1,12 @@
 """Ta class which contains functions for calculating technical indicators."""
 from typing import Callable
 
+from pure_ta.alma import get_alma
 from pure_ta.ema import get_ema
 from pure_ta.sma import get_sma
 
 
-def _validate_arg(indicator: str, value: int, min_value: int):
+def _validate_arg(indicator: str, value: int, min_value: int = 1):
     if value < min_value:
         raise ValueError(
             f"""LookBack must be greater than {min_value}
@@ -20,11 +21,23 @@ class Ta:
     @staticmethod
     def sma(length: int = 20) -> Callable[[float], float]:
         """returns a function that calculates the simple moving average"""
-        _validate_arg("SMA (Simple Moving Average)", length, 1)
+        _validate_arg("SMA (Simple Moving Average)", length)
         return get_sma(length)
 
     @staticmethod
     def ema(length: int = 20) -> Callable[[float], float]:
         """returns a function that calculates the exponential moving average"""
-        _validate_arg("EMA (Exponential Moving Average)", length, 1)
+        _validate_arg("EMA (Exponential Moving Average)", length)
         return get_ema(length)
+
+    @staticmethod
+    def alma(
+        length: int = 20, sigma: float = 6, offset: float = 0.85
+    ) -> Callable[[float], float]:
+        """returns a function that calculates the Arnaud Legoux Moving Average"""
+        _validate_arg("ALMA (Arnaud Legoux Moving Average)", length)
+        if sigma < 1:
+            raise ValueError("Sigma must be greater than 1 to calculate the ALMA")
+        if offset < 0 or offset > 1:
+            raise ValueError("Offset must be between 0 and 1 to calculate the ALMA")
+        return get_alma(length, sigma=sigma, offset=offset)
---
+++ tests/test_alma.py
@@ -0,0 +1,32 @@
+"""expected results.
+ * https://docs.google.com/spreadsheets/d/1T14VAhzM14Yqf4sjE7UEcl1yJUyS27TRLPf0Bg-n3BY/edit?usp=sharing"""  # noqa: E501
+import math
+
+from pure_ta import Quote, Ta
+
+
+def test_alma_returns_correct_number_of_results(get_default: list[Quote]):
+    """alma results should have the correct length."""
+    alma = Ta.alma()
+    results = [alma(q.close) for q in get_default]
+    assert len(results) == 502
+
+
+def test_alma_returns_correct_number_of_results_without_nan(get_default: list[Quote]):
+    """alma results should return the correct number of results without nan."""
+    alma = Ta.alma(length=10)
+    results = [alma(q.close) for q in get_default]
+    results_without_nan = [r for r in results if not math.isnan(r)]
+    assert len(results_without_nan) == 493
+
+
+def test_alma_returns_correct_calculation_results(get_default: list[Quote]):
+    """alma results should return the correct calculation results."""
+    alma = Ta.alma(length=10)
+    results = [alma(q.close) for q in get_default]
+    assert math.isnan(results[0])
+    assert math.isnan(results[8])
+    assert round(results[9], 4) == 214.1839
+    assert round(results[24], 4) == 216.0619
+    assert round(results[249], 4) == 257.5787
+    assert round(results[501], 4) == 242.1871
